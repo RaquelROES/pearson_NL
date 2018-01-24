9f219ab5c492f1da0018934653407231115c6d18
RaquelROES <32135998+RaquelROES@users.noreply.github.com>
2018-01-24 12:06:38 +0100

    Update style.js

--- style.js
+++ style.js
@@ -1,15 +1,15 @@
 (function (blink) {
 	'use strict';
 
-	var pearson_NL_demo = function () {
+	var pearson_NLStyle = function () {
 			blink.theme.styles.basic.apply(this, arguments);
 		},
 		page = blink.currentPage;
 
-	pearson_NL.prototype = {
-		bodyClassName: 'content_type_clase_lnplat',
+	pearson_NLStyle.prototype = {
+		bodyClassName: 'content_type_clase_pearson_NL',
 		ckEditorStyles: {
-			name: 'lnplat',
+			name: 'pearson_NL',
 			styles: [
 				{ name: 'Título 1', element: 'h2', attributes: { 'class': 'bck-title1'} },
 				{ name: 'Título 2', element: 'h3', attributes: { 'class': 'bck-title2'} },
@@ -56,8 +56,8 @@
 				{ name: 'Escuchar', type: 'widget', widget: 'blink_box', attributes: { 'class': 'escuchar-box' } },
 				{ name: 'Actividad Scorm ', type: 'widget', widget: 'blink_box', attributes: { 'class': 'actividad-scorm-box' } },
 
-				{ name: 'Desplegable', type: 'widget', widget: 'blink_dropdown', attributes: { 'class': 'lnplat-dropdown' } },
-				{ name: 'Desplegable 2', type: 'widget', widget: 'blink_dropdown', attributes: { 'class': 'lnplat-dropdown-2' } },
+				{ name: 'Desplegable', type: 'widget', widget: 'blink_dropdown', attributes: { 'class': 'pearson_NL-dropdown' } },
+				{ name: 'Desplegable 2', type: 'widget', widget: 'blink_dropdown', attributes: { 'class': 'pearson_NL-dropdown-2' } },
 
 				{ name: 'Imagen Sin Bordes', type: 'widget', widget: 'image', attributes: { 'class': 'normal-img' } },
 				{ name: 'Imagen derecha', element: 'img', attributes: { 'class': 'bck-img right' } },
@@ -163,7 +163,7 @@
 					'</div>';
 
 			$navbarBottom
-				.attr('class', 'lnplat-navbar')
+				.attr('class', 'pearson_NL-navbar')
 				.wrapInner('<div class="navbar-content"></div>')
 				.find('ol')
 					.before(dropDown)
@@ -325,7 +325,7 @@
 
 		animateNavbarOnScroll: function () {
 			if (!blink.isApp) return;
-			var $navbar = $('.lnplat-navbar');
+			var $navbar = $('.pearson_NL-navbar');
 			var lastScrollTop = 0;
 			$('.js-slider-item').scroll(function () {
 				var scrollTop = $(this).scrollTop();
@@ -335,14 +335,14 @@
 		},
                 
                 changeHighBar: function () {
-                    if($('.lnplat-navbar').length>0 && $('.navbar').length>0){
-                        blink.theme.setTopByHeight('.navbar', '.lnplat-navbar');
+                    if($('.pearson_NL-navbar').length>0 && $('.navbar').length>0){
+                        blink.theme.setTopByHeight('.navbar', '.pearson_NL-navbar');
                     }
                 }
 	};
 
-	pearson_NL.prototype = _.extend({}, new blink.theme.styles.basic(), pearson_NL.prototype);
+	pearson_NLStyle.prototype = _.extend({}, new blink.theme.styles.basic(), pearson_NLStyle.prototype);
 
-	blink.theme.styles['lnplat'] = pearson_NL;
+	blink.theme.styles['pearson_NL'] = pearson_NLStyle;
 
 })( blink );
